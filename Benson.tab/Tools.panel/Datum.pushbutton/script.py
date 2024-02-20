73c2b6e948393cf7c9e85d2c4c408095739814d9
PeteHeibel <peterjamesheibel@gmail.com>
2024-02-20 13:55:16 -0800

    added Datum pushbutton

---
+++ Benson.tab/Tools.panel/Datum.pushbutton/script.py
@@ -0,0 +1,26 @@
+from pyrevit import revit, DB, forms
+
+# Get the active view
+view = revit.doc.ActiveView
+
+# Name of the subcategory to turn off
+subCategory = "Datum Symbol"
+
+if str(view.DetailLevel) == "Coarse":
+    try:
+        # Get a subcategory of the Detail Item model category by name
+        category = revit.doc.Settings.Categories.get_Item(DB.BuiltInCategory.OST_DetailComponents).SubCategories.get_Item(subCategory);
+
+        # Wrap the visibility toggle in a transaction
+        with revit.Transaction("Toggle Datum Symbol Visibility"):
+            # Get the current hidden state and toggle directly
+            new_hidden = not view.GetCategoryHidden(category.Id)
+            view.SetCategoryHidden(category.Id, new_hidden)
+
+            # Display result to user
+            # forms.alert(category.Name + " visibility is " + str(new_hidden), title="Result", warn_icon=False)
+    except:
+        forms.alert(subCategory + "s could not be found in this document.", title="Result", warn_icon=False)
+
+else:
+    forms.alert("Datum Symbols are only visible in views with a 'Coarse' Detail Level.")
