27d48c01378865beedab0ee170b2c0c3c54c52c2
PeteHeibel <peterjamesheibel@gmail.com>
2024-03-21 14:34:09 -0700

    fix: add try/except to Dynamo scripts folder launch

--- Benson.tab/System.panel/Folders.stack/Dynamo Scripts.pushbutton/script.py
+++ Benson.tab/System.panel/Folders.stack/Dynamo Scripts.pushbutton/script.py
@@ -1,5 +1,8 @@
-"""Opens the Revit 'Journals' folder in File Explorer"""
+"""Opens the Dynamo 'Scripts' folder in File Explorer"""
 
 from pyrevit import coreutils
 
-coreutils.open_folder_in_explorer("K:\Revit Standards\Dynamo\Scripts")
+try:
+    coreutils.open_folder_in_explorer("K:\Revit Standards\Dynamo\Scripts")
+except OSError:
+    pyrevit.forms.alert("K: drive not connected. Folder could not be opened.", "Error")
