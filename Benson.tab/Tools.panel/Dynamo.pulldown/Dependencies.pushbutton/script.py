e9aed25899a818dcff7d3d8cdcdf1a25b705f40c
PeteHeibel <peterjamesheibel@gmail.com>
2024-03-21 14:18:23 -0700

    feat: update 'Dependencies' script docs: update 'Dependencies' toolip

--- Benson.tab/Tools.panel/Dynamo.pulldown/Dependencies.pushbutton/script.py
+++ Benson.tab/Tools.panel/Dynamo.pulldown/Dependencies.pushbutton/script.py
@@ -1,32 +1,9 @@
-from pyrevit import revit, HOST_APP
+import pyrevit
+from pyrevit import forms
 import subprocess
 
-source_directory = ""
-destination_directory = ""
-
-# create code that copies Dynamo packages from network to local machine
-# replace version if already exists, but do not overwrite in case others have custom configurations
-# include a try/except statement that checks if the K:/ drive exists (where scripts are stored)
-
-# review for proper function:
-# https://learn.microsoft.com/en-us/windows-server/administration/windows-commands/robocopy
-
-
-
-
-
-
-
-# _____________________________________________________________________________
-
-# not sure I need to do this if I just copy all packages/folders at once
-# even if they've never opened Dynamo, it should work to just have the folder waiting
-
-# if HOST_APP.version == 2021:
-#     pass
-# elif HOST_APP.version == 2022:
-#     pass
-# elif HOST_APP.version == 2023:
-#     pass
-# elif HOST_APP.version == 2024:
-#     pass
+try:
+    subprocess.call([r'K:\Revit Standards\Dynamo\Dependencies\Download Dependencies.bat'])
+    pyrevit.forms.alert("Beep boop beep... \n\nDynamo now has everything it needs to run smoothly.", "Result", warn_icon=False)
+except OSError:
+    pyrevit.forms.alert("K: drive not connected. Dependencies could not be downloaded.", "Error")
