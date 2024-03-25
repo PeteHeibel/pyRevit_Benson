0c75d2c5bcd80115a962dadcbe7c6e8f07789a0e
PeteHeibel <peterjamesheibel@gmail.com>
2024-03-25 15:38:37 -0700

    fix: run doc-opened hook only on Benson projects that are cloud hosted

--- hooks/doc-opened.py
+++ hooks/doc-opened.py
@@ -1,4 +1,4 @@
-from pyrevit import revit, DB, HOST_APP
+from pyrevit import revit, DB, HOST_APP, forms
 import os
 
 import sys
@@ -20,8 +20,8 @@ for sheet in sheets:
         hasHomeView = True
         break
 
-# If the project contains a sheet titled "Home", run the script
-if hasHomeView:
+# If the project contains a sheet titled "Home" and the project is hosted in the cloud, run the script
+if hasHomeView and doc.IsModelInCloud:
     trans = DB.Transaction(doc)
     trans.Start("Write Home Data")
 
@@ -29,10 +29,25 @@ if hasHomeView:
     revit_version_parameter = doc.ProjectInformation.LookupParameter("Revit Version")
     revit_version_parameter.Set(revit_subversion)
 
+    # Get Revit version and local cache path
+    revit_version = str(HOST_APP.version)
+    local_app_data = os.getenv('LOCALAPPDATA')
+    collaboration_cache_path = os.path.join(local_app_data, "Autodesk\Revit\Autodesk Revit " + revit_version + "\CollaborationCache")
+
+    # Extract cloud model path information
+    cloud_file_path = doc.GetCloudModelPath()
+    account_guid = "CFP2XTS6R29ZTP5B"
+    project_guid = cloud_file_path.GetProjectGUID()
+    model_guid = cloud_file_path.GetModelGUID()
+
+    # Construct collaboration cache file path efficiently
+    collaboration_cache_project_path = os.path.join(collaboration_cache_path, account_guid, str(project_guid))
+    cloud_file_cache_path = os.path.join(collaboration_cache_project_path, str(model_guid) + ".rvt")
+
     # Get and set the "File Size" project parameter
     file_size_parameter = doc.ProjectInformation.LookupParameter("File Size")
-    file_path = doc.PathName
-    file_size_bytes = float(os.path.getsize(file_path))
+
+    file_size_bytes = float(os.path.getsize(cloud_file_cache_path))
     file_size_mb = file_size_bytes / 1024**2
     file_size_parameter.Set(file_size_mb)
 
@@ -44,3 +59,7 @@ if hasHomeView:
     # additional parameter writing here
 
     trans.Commit()
+
+else:
+    # Do nothing
+    pass
