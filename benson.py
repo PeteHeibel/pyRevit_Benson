c8f554822a8e7d72dd446e861b638adce34ce840
PeteHeibel <peterjamesheibel@gmail.com>
2024-06-07 13:51:38 -0700

    updated revit subversions

--- benson.py
+++ benson.py
@@ -1,16 +1,16 @@
 """list of functions used in Benson Revit addins"""
 
-
 def latest_version(revit_version):
     if revit_version == '2021':
-        # actual version is 2021.1.90 but subversion reports the following
         return '2021.2'
     elif revit_version == '2022':
-        return '2022.1.5'
+        return '2022.1.6'
     elif revit_version == '2023':
-        return '2023.1.30'
+        return '2023.1.40'
     elif revit_version == '2024':
-        return '2024.2'
+        return '2024.2.10'
+    # elif revit_version == '2025':
+    #     return '2025.1'
     else:
         warning = 'Revit {} is not supported at Benson. Please upgrade.'.format(revit_version)
         return warning
