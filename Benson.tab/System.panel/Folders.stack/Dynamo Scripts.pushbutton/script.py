7e58f165d861be5faf97ab03677ee5dff1b8586e
PeteHeibel <peterjamesheibel@gmail.com>
2024-06-28 11:58:04 -0700

    updated Dynamo Scripts pushbutton

--- Benson.tab/System.panel/Folders.stack/Dynamo Scripts.pushbutton/script.py
+++ Benson.tab/System.panel/Folders.stack/Dynamo Scripts.pushbutton/script.py
@@ -1,8 +1,22 @@
-"""Opens the Dynamo 'Scripts' folder in File Explorer"""
+# -*- coding: utf-8 -*-
+# script metadata
+__title__   = 'Dynamo Scripts'
+__doc__     = "Opens the Dynamo 'Scripts' folder in File Explorer"
+__author__  = 'Pete Heibel'
+__min_revit_year__  = 2021
+__max_revit_year__  = 2024
+__context__ = 'zero-doc'
 
-from pyrevit import coreutils
+# imports
+import os
+import pyrevit
+from pyrevit import coreutils, forms
 
-try:
-    coreutils.open_folder_in_explorer("K:\Revit Standards\Dynamo\Scripts")
-except OSError:
+# variables
+path = "K:\Revit Standards\Dynamo\Scripts"
+
+# main
+if os.path.exists(path):
+    coreutils.open_folder_in_explorer(path)
+else:
     pyrevit.forms.alert("K: drive not connected. Folder could not be opened.", "Error")
